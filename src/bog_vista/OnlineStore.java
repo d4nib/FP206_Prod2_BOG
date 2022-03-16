6342524a3623d2aa17ccd684b6cfbacbc6f0fc2d
d4nib <dblancoalb@uoc.edu>
2022-03-16 20:33:49 +0100

    Change app class

--- src/bog_vista/OnlineStore.java
+++ src/bog_vista/OnlineStore.java
@@ -1,4 +1,5 @@
-public class App {
+package bog_vista;
+public class OnlineStore {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
